8396d5d2ed1c4497a0d8cddd7c3120510cb77469
Briell Quintana <brielluh@gmail.com>
2025-05-14 15:18:09 -0400

    refactor: restructure image display and update related text alignment

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -11,12 +11,17 @@ const Start = ({ text, dispatch }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center p-4 space-y-10 text-white">
       <h1 className="text-3xl font-bold text-center text-slate-300">{text}</h1>
-      <img
-        src={image}
-        className="w-1/2 mb-2 overflow-hidden transition duration-300 select-none h-1/2 rounded-2xl hover:scale-170 active:scale-170"
-        onMouseEnter={() => dispatch({ type: "ImgTouched" })}
-      />
-      <p className="my-t-0">(mantén presionada la foto)</p>
+      <div className="flex items-center justify-center select-none">
+        <img
+          src={image}
+          className="w-1/2 mb-2 transition duration-300 cursor-pointer select-none h-1/2 rounded-2xl hover:scale-170 active:scale-170"
+          onMouseEnter={() => dispatch({ type: "ImgTouched" })}
+        />
+      </div>
+      <p className="my-0">(mantén presionada la foto)</p>
+      <p className="mt-0">
+        por cierto perdón por la foto, no tenia mas ninguna ksjksjksj
+      </p>
       <Button
         onClick={() => dispatch({ type: "QuizStart" })}
         variant="contained"
